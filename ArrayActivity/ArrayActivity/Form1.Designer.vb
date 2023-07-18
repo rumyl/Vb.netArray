3caf5bc17fdf1f3ea77187fb9497ef90227aa9b1
Rumyl James B. Dela Cruz <131506677+rumyl@users.noreply.github.com>
2023-07-18 10:52:42 +0800

    Add files via upload

---
+++ ArrayActivity/ArrayActivity/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(30, 57)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 25
+        Me.DataGridView1.Size = New System.Drawing.Size(305, 200)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(30, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(125, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "VB.net Array"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(368, 290)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Activity 5"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label1 As Label
+End Class
---
+++ ArrayActivity/ArrayActivity/Form1.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Data
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim myArray(,) As Object = {
+            {"Jasmine", 20, "Female"},
+            {"Jocelyn", 20, "Female"},
+            {"Jessa", 20, "Female"},
+            {"Mitch", 20, "Female"},
+            {"Abet", 30, "Male"},
+            {"Angelo", 40, "Male"}
+        }
+        DisplayArrayInDataGridView(myArray)
+    End Sub
+
+    Private Function ConvertArrayToDataTable(ByVal dataArray As Object(,)) As DataTable
+        Dim dataTable As New DataTable()
+
+        ' Set the column headers
+        dataTable.Columns.Add("Name")
+        dataTable.Columns.Add("Age")
+        dataTable.Columns.Add("Gender")
+
+        ' ma add raun kita it Rows sa Data Table
+        Dim rowsCount As Integer = dataArray.GetLength(0)
+        Dim columnsCount As Integer = dataArray.GetLength(1)
+
+        For i As Integer = 0 To rowsCount - 1
+            Dim rowValues(columnsCount - 1) As Object
+            For j As Integer = 0 To columnsCount - 1
+                rowValues(j) = dataArray(i, j)
+            Next
+            dataTable.Rows.Add(rowValues)
+        Next
+
+        Return dataTable
+    End Function
+
+    Private Sub DisplayArrayInDataGridView(ByVal dataArray As Object(,))
+
+        Dim dataTable As DataTable = ConvertArrayToDataTable(dataArray)
+
+        DataGridView1.DataSource = dataTable
+    End Sub
+
+End Class
